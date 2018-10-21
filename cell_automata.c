22cf0df4ea03f0f99b96121fcb8cd935a262da95
Felix Jackson <fojackson1@gmail.com>
2018-10-21 21:54:36 +0100

    added in fullness_array

--- cell_automata.c
+++ cell_automata.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 // Does this change things? //
 
@@ -12,29 +13,33 @@
 // I is always grid length (up down), J is always grid width (left right)
 
 
-int find_free_space(int argc, char* argv[])
+char* find_free_space(double** food_array, double** fullness_array,double food_eat_rate, int grid_length, int grid_width, int i, int j)
 {
-	int array_length = (int)argv[1]
-	int array_width = (int)argv[2]
-	int i = (int)argv[3]
-	int j = (int)argv[4]
 
-	// printf("Set array length, then array width, then i, then j\n");
-	// scanf("%d \n %d \n %d \n %d \n ",&array_length,&array_width,&i,&j);
-
-	printf("Array length = %d\n", array_length);
-	printf("Array width = %d\n", array_width);
+	printf("Function find_free_space received the following params:\n");
+	printf("Array length = %d\n", grid_length);
+	printf("Array width = %d\n", grid_width);
 	printf("I = %d\n", i);
 	printf("J = %d\n", j);
+	int counter;
+	double no_food = 0.0;
 
-	char directions[4];
 
-	assert(i < array_length && "i out of bounds");
-	assert(j < array_width && "j out of bounds");
+	assert(i < grid_length && "i out of bounds");
+	assert(j < grid_width && "j out of bounds");
 
+	char* directions = (char*)malloc(4*sizeof(char));
+
+
+	counter = 0; // Reinitialise counter each time
 	if  (j-1 >= 0)
 	{
 		directions[0] = 'L';
+
+		if (food_array[i][j-1] > no_food){
+		food_array[i][j-1] = food_array[i][j-1] - food_eat_rate;  
+		counter +=1;
+		}
 	}
 	else 
 	{
@@ -42,9 +47,15 @@ int find_free_space(int argc, char* argv[])
 	}
 
 
-	if  (j+1 <= array_width)
+	if  (j+1 < grid_width)
 	{
 		directions[1] = 'R';
+		
+		if (food_array[i][j+1] > no_food)
+		{
+		food_array[i][j+1] = food_array[i][j+1] - food_eat_rate;
+		counter +=1;
+		}
 	}
 	else 
 	{
@@ -55,29 +66,35 @@ int find_free_space(int argc, char* argv[])
 	if  (i-1 >= 0)
 	{
 		directions[2] = 'U';
+		if (food_array[i-1][j] > no_food)
+		{
+		food_array[i-1][j] = food_array[i-1][j] - food_eat_rate;
+		counter +=1;
+		}
 	}
 	else 
 	{
 		directions[2] = 'X';
 	}
 
-	if  (i+1 <= array_length)
+	if  (i+1 < grid_length)
 	{
 		directions[3] = 'D';
+		if (food_array[i-1][j] > no_food)
+		{
+		food_array[i+1][j] = food_array[i+1][j] - food_eat_rate;
+		counter +=1;
+		}
 	}
 	else 
 	{
 		directions[3] = 'X';
 	}
 
-	int k;
-	for (k=0;k<4;k++)
-	{
-		printf("%c \n", directions[k]);
-	}
-	
-
+	fullness_array[i][j] += 0.1*counter;
+	return(directions);
 }
+
 
 
 
@@ -105,7 +122,7 @@ int main(void)
 	
 	}
 
-	for (i=0;i<grid_width;i++)
+	for (i=0;i<grid_length;i++)
 	{
 		for (j=0;j<grid_width;j++)
 		{
@@ -126,7 +143,6 @@ int main(void)
 	for (i=0;i<grid_length;i++)
 	{
 		food_array[i] = (double*)malloc(grid_length * sizeof(int)); // each column of array is now an address pointing to a 4 byte location in memory, where we'll store some ints
-	int i,j;
 	}
 
 
@@ -139,20 +155,49 @@ int main(void)
 	}
 
 
+	// Now build fullness array
+
+	double** fullness_array = (double**)malloc(grid_width * sizeof(double*)); // pointer to a pointer to an array
+	
+	for (i=0;i<grid_length;i++)
+	{
+		fullness_array[i] = (double*)malloc(grid_length * sizeof(int)); // each column of array is now an address pointing to a 4 byte location in memory, where we'll store some ints
+	}
+
+	for (i=0;i<grid_length;i++)
+	{
+		for (j=0;j<grid_width;j++)
+		{
+			fullness_array[i][j]= 1.0;
+		}
+	}
+
+
+
+	
+
+	// TEMP - DELETE 
+		printf("Food array updated as follows:\n");
+		for (i=0;i<grid_length;i++)
+		{
+			for (j=0;j<grid_width;j++)
+			{
+				//food_array[i][j] += food_growth_rate
+				printf("%f ", food_array[i][j]);
+			}
+			printf("\n");
+		}
+
+
+
 	// Test array has been built properly
-	printf("Food Array element 2,1  = %f\n", food_array[2][1]);
+	printf("Food Array element 4,3  = %f\n", food_array[4][3]);
 
-
-	
-	
-
-
-
-	// ---------- //
+	// ----------------------------------------------------------------//
 
 	/* ADD AGENTS 	*/
 
-	// Start by allocating 5 in the centre 
+	// Start by simply placing 5 in the centre 
 	// First find centre, and logic to build + shape distribution around it
 
 	double grid_width_double = (double) grid_width;
@@ -163,9 +208,9 @@ int main(void)
 
 	animal_array[length_centre][width_centre] = 1;
 
-	for (i=0;i<grid_width;i++)
+	for (i=0;i<grid_length;i++)
 	{
-		for (j=0;j<grid_length;j++)
+		for (j=0;j<grid_width;j++)
 		{
 			if ((abs(j-width_centre) < 1 && abs(i - width_centre) <= 1) || (abs(j-width_centre) <= 1 && abs(i - width_centre) < 1))
 			{	
@@ -177,7 +222,8 @@ int main(void)
 
 
 
-// Print array to check progress
+// Print animal_array to check progress
+	printf("Animal grid currently looks like this:\n");
 	for (i=0;i<grid_length;i++)
 	{
 		for (j=0;j<grid_width;j++)
@@ -187,10 +233,16 @@ int main(void)
 		printf("\n");
 	}
 
+	printf("\n-------------------------------------------------------------\n\n");
 
+
+
+	// ----------------------------------------------------------------//
 
 
 	/* RUN THE GAME  */
+
+
 	/* First, I'll assume that animals are alive as long as they can feed each turn. */
 	/* As soon as they can't feed, they die. This is basically saying that their feed rate*/ 
 	/* is much faster than a single time interval*/
@@ -199,34 +251,68 @@ int main(void)
 	
 	// Set constants
 	double food_growth_rate = 0.2;
-	double food_eat_rate = 0.1
+	double food_eat_rate = 0.1;
 
 	// For each agent, need to determine n_Free_cells surrounding it. Then divide the
 	// total eat rate it requires by n_Free_cells
 
-	int t = 0;
+	// Make space for a char array called directions
+	char* directions;
+
+
+	int t = 0; // time variable to track game progress
+	int k; // just a loop variable to print
+
 	// Begin time simulation 
-	while (t< 100)
+	while (t< 1)
 	{  
 		for (i=0;i<grid_length;i++) // First loop through the agents and find free space
 		{
 			for (j=0;j<grid_width;j++) 
 			{
-				if 
+				if (animal_array[i][j] == 1)
+				{
+					// Find space around each position
+					directions = find_free_space(food_array,fullness_array,food_eat_rate,grid_length,grid_width,i,j);
+					printf("\nDirections position %d %d can move are:\n",i,j);
+					
+					for (k=0;k<4;k++)
+					{
+						printf("%c ", directions[k]);
+					}
+					printf("\n ----------------------------------------------------\n");
+
+					// Kill off any starved animals
+					if (fullness_array[i][j] < 0.5)
+					{
+						animal_array[i][j] = 0;
+					}
+				}
 			}
 
 		}
-		
 
+		// Grow food each turn
+		printf("Food array updated as follows:\n");
+		for (i=0;i<grid_length;i++)
+		{
+			for (j=0;j<grid_width;j++)
+			{
+				//food_array[i][j] += food_growth_rate
+				printf("%f ", food_array[i][j]);
+			}
+			printf("\n");
+		}
 
 		t+=1;
 	}
 
 
-
+	free(food_array);
+	free(animal_array);
+	free(fullness_array);
 		
 
 }
-
 
 
